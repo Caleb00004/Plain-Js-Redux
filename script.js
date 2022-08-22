b9509c792c90ee5b0a01afc2ceb807709894f42a
caleb <calebakpan7@gmail.com>
2022-08-22 14:47:26 +0100

    initialised Project

---
+++ script.js
@@ -0,0 +1,9 @@
+//import { createStore } from 'redux'
+import reduxjsToolkit from 'https://cdn.skypack.dev/@reduxjs/toolkit';
+
+console.log("console.log")
+
+const action = {
+    type: "Increment"
+}
+
