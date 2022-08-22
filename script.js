c9c7e4bedd6449f2e2b430cbf845acc798973717
caleb <calebakpan7@gmail.com>
2022-08-22 18:50:08 +0100

    made more dynamic action to add and reduce state

--- script.js
+++ script.js
@@ -1,19 +1,27 @@
-console.log("console.log")
-
-/*
-const action = {
-    type: "Increment"
-} */
-
 // Note: // Naming convention, the type name should be in Upper case
-// This function returns 'increment' action. \\ remember actions are objects.
-function increment() {
+
+/* 
+switch syntax
+
+switch(condition) {
+    case (condition is '1') :
+        code.....
+    case (condition is '2') :
+        code...
+    case....
+}
+*/
+
+
+// created functions to return action objects. \\ remember actions are objects.
+function increment(amount) {
     return {
-        type: 'INCREMENT'
+        type: 'INCREMENT',
+        payLoad: amount 
     }
 }
 
-
+// decrement action function
 function decrement() {
     return {
         type: 'DECREMENT'
@@ -34,12 +42,18 @@ function reducer(state = {count: 0}, action) {
 }
 */
 
-// using a switch statement for the reducer
-function reducerSwitch(state = {count: 1}, action) {
+
+/* 
+// using a switch statement for the reducer 
+function reducerSwitch(state = {count: 0}, action) {
     switch(action.type) {
+        case ("CHANGE_COUNT") :
+            return {
+                count: state.count + action.payLoad
+            }
         case ("INCREMENT") : //increment state to increase the count state if the action type is Increment.
             return {
-                count : state.count + 1
+                count : state.count + action.payLoad
             }
         case ("DECREMENT") :
             return {
@@ -49,9 +63,29 @@ function reducerSwitch(state = {count: 1}, action) {
             return state 
     }
 }
+*/
+
+// A More dynamic action to both increment and Decrement state.
+function changeCount(number) {
+    return {
+        type: "CHANGE_COUNT",
+        payLoad: number
+    }
+}
+
+// New Reducer for the changeCount action.
+function newReducer(state = {count : 0}, action) {
+    switch (action.type) {
+        case ("CHANGE_COUNT") :
+            return {
+                count: state.count + action.payLoad
+            }
+    }
+}
 
 // creating Redux Store. Takes the reducer as parameter.
-const store = Redux.createStore(reducerSwitch)
+//const store = Redux.createStore(reducerSwitch)
+const store = Redux.createStore(newReducer)
 console.log(store)
 
 // runs anytime an action is dispatched. i.e a state changes.
@@ -59,19 +93,10 @@ store.subscribe(() => {
     console.log(store.getState()) // calling getState to see the current state after change.
 })
 
-// sending the increment action to the reducer
-store.dispatch(increment())
+// sending the actions to the reducer
+store.dispatch(changeCount(-5))
+
+//store.dispatch(decrement())
 
 
 
-/* 
-switch syntax
-
-switch(condition) {
-    case (condition is '1') :
-        code.....
-    case (condition is '2') :
-        code...
-    case....
-}
-*/
